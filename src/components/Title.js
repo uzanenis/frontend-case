4e4712fea7437f4befba134aed72718dd5cf3f37
uzanenis <uzanenis@outlook.com>
2023-01-17 14:27:47 +0300

    a tag href removed

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
-    return (
-        <div className="component-title">
-            <div className="title-01">
-                Everyday items, we have something to suit every occasion.
-            </div>
-            <div className="subtitle-01">
-                At suspendisse augue lectus arcu, accumsan ut sit posuere vitae sit tincidunt semper eu proin leo
-                gravida cursus.
-            </div>
-            <a href="#" className="button-01">
-                Shop all everyday items
-            </a>
-        </div>
-    );
+  return (
+    <div className="component-title">
+      <div className="title-01">
+        Everyday items, we have something to suit every occasion.
+      </div>
+      <div className="subtitle-01">
+        At suspendisse augue lectus arcu, accumsan ut sit posuere vitae sit
+        tincidunt semper eu proin leo gravida cursus.
+      </div>
+      <div className="button-01">Shop all everyday items</div>
+    </div>
+  );
 };
 
 export default Title;
